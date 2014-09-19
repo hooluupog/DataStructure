1ea7b1d7c92d95bfc5640acdba9d67b266ab7d85
hooluupog <QQ510371827@gmail.com>
2014-09-19 16:28:20 +0800

    Create HeapSort.go

---
+++ DataStructure/src/HeapSort.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func HeapSort(b []int) {
+	// Consider b to be a commplete binary tree.
+	// Build heap with greatest element on the top.
+	for i := (len(b) - 1) / 2; i > 0; i-- {
+		HeapAdjust(b, i)
+	}
+	// Continually Pop heap top elements and adjust remaining elements to be a heap.
+	for i := len(b) - 1; i > 1; i-- {
+		//swap the top element with the last element on the heap.
+		b[i], b[1] = b[1], b[i]
+		// Generate new heap.
+		HeapAdjust(b[:i], 1)
+	}
+}
+
+func HeapAdjust(b []int, i int) {
+	// Siftdown i
+	siftdata := b[i] //Save the element to be siftdown.
+	for child := 2 * i; child <= len(b)-1; child = 2 * child {
+		//Siftdown along with the bigger child element branch.
+		if child < len(b)-1 && b[child] < b[child+1] {
+			child++
+		}
+		if siftdata >= b[child] {
+			break
+		} else {
+			b[i] = b[child] // Adjust b[child] to parent node.
+			i = child       // Update i for continuing siftdown.
+		}
+	}
+	b[i] = siftdata // Put the siftdata at the final position.
+}
+
+func IsHeap(b []int) bool { // If s is a big endian heap.
+	length := len(b) - 1
+	if length%2 == 0 { // Len(b)-1 is even number. b contains a single-branch node.
+		if b[length/2] < b[length] {
+			return false
+		}
+		for i := length/2 - 1; i >= 1; i-- {
+			if b[i] < b[2*i] || b[i] < b[2*i+1] {
+				return false
+			}
+		}
+	} else { // Len(b)-1 is odd number,none single-branch node.
+		for i := length / 2; i >= 1; i-- {
+			if b[i] < b[2*i] || b[i] < b[2*i+1] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	var a int
+	var l []int
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	for {
+		_, err := fmt.Fscan(r, &a)
+		if err == io.EOF {
+			break
+		}
+		l = append(l, a)
+	}
+	if !IsHeap(l) {
+		HeapSort(l)
+		fmt.Fprintln(w, l[1:])
+		w.Flush()
+	} else {
+		fmt.Println(l[1:], "is already a heap.")
+	}
+}
