41b814eaf70d86b80743db6ed1e8a8b4e23b451d
hooluupog <QQ510371827@gmail.com>
2014-09-21 11:19:31 +0800

    Create Select_HeapSort.c

---
+++ DataStructure/Algorithm_SingleFile/快速排序及其划分算法的应用/Select_HeapSort.c
@@ -0,0 +1,46 @@
+/* 从一个大小为n的数组中找出前k大的元素 */
+
+#include "stdio.h"
+
+int a[1000001];
+void SiftDown(int* list,int root, int end) {
+	int child;
+	int siftdata;
+	siftdata = list[root];
+	for(child = 2*root; child <= end; child = 2*child) {
+		if(child < end && list[child] < list[child + 1]) 
+			child ++;
+		if(siftdata >= list[child])
+			break;
+		list[root] = list[child];
+		root = child;
+	}
+	list[root] = siftdata;
+}
+
+void HeapSort(int* list,int m) {
+	int i,temp;
+	for(i = list[0]/2; i >= 1; i--) {
+		SiftDown(list,i,list[0]);
+	}
+	for(i = a[0]; i > a[0] - m; i--) {
+		temp = list[i];
+		list[i] = list[1];
+		list[1] = temp;
+		SiftDown(list,1,i-1);
+	}
+}
+
+void main(){
+	int n,m,i = 1;
+	scanf("%d %d\n",&n,&m);
+	a[0] = n;
+	while(n--){
+		scanf("%d",&a[i++]);
+	}
+	HeapSort(a,m);
+	for(i = 1;i <= m-1;i++){
+		printf("%d ",a[a[0] - i + 1]);
+	}
+	printf("%d\n",a[a[0] - i + 1]);
+}
