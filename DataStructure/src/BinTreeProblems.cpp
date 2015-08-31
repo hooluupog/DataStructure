cbd34af1b38ad5fb43c1eb5230fc014a325d8d3d
hooluupog <qq510371827@gmail.com>
2015-08-31 22:21:38 +0800

    add new

--- DataStructure/src/BinTreeProblems.cpp
+++ DataStructure/src/BinTreeProblems.cpp
@@ -17,12 +17,25 @@
  *    \   \
  *    3    3
  *
+ * 求二叉树的镜像；
+ * 判断一棵树是否为另一颗树的子结构，如下图，B为A的子结构
+ *          1                        8
+ *        /   \                     /  \
+ *       8     7                   9    2
+ *     /   \
+ *    9     2
+ *         /  \
+ *        4    7
+ *        (A)                        (B)
+ *
  * 判断是否平衡2叉树
+ * 最近的公共祖先结点
  */
 
 #include "stdio.h"
 #include "BinTree.h"
 #include "stdlib.h"
+#include <vector>
 
 int max(int a, int b) {
 	return a > b ? a : b;
@@ -89,9 +102,87 @@ bool IsSymmetricTree(BinTree r){
  	return Symmetric(r->lchild,r->rchild);
 }
 
+//输出树的镜像
+void MirrorTree(BinTree r){
+	if (r == NULL) return;
+	BinNode *temp;
+	temp = r->lchild;
+	r->lchild = r->rchild;
+	r->rchild = temp;
+	MirrorTree(r->lchild);
+	MirrorTree(r->rchild);
+}
+
+bool doesHaveAllNodes(BinTree a,BinTree b){ // 以N为根结点遍历a，判断a是否含有和b相同结构的结点
+	if(b == NULL) return true;  //到达b的叶子结点，说明具有相同结构，终止递归；
+	if(a == NULL) return false; //到达a的叶子结点，说明不具有相同结构，终止递归；
+	if(a ->data != b->data) return false; //结点值不相同，不具有相同结构，终止递归；
+	return doesHaveAllNodes(a->lchild,b->lchild) && doesHaveAllNodes(a->rchild,b->rchild);
+}
+bool hasSubTreeRoot(BinTree a,BinTree b){ //判断b的根结点是否存在于a中，如果存在设此结点为N
+	bool result = false;
+	if (a->data == b->data)
+		result = doesHaveAllNodes(a, b);
+	if (!result && a->lchild != NULL)
+		result = hasSubTreeRoot(a->lchild, b);
+	if (!result && a->lchild != NULL)
+		result = hasSubTreeRoot(a->rchild, b);
+	return result;
+}
+bool IsSubTree(BinTree a,BinTree b){ //判断b是否为a的子结构
+	if (a == NULL && b == NULL) return true;
+	if ((a == NULL && b != NULL) || (a != NULL && b == NULL)) return false;
+	return hasSubTreeRoot(a,b);
+}
+
+//找出根结点到所要查询的结点的路径存储到链表中
+bool GetNodePath(BinTree r,int v,std::vector<BinNode*> &list){
+	if (ToInt(r) == v) {
+		return true;
+	}
+	list.push_back(r);
+	bool found = false;
+	if(r->lchild != NULL){
+		found = GetNodePath(r->lchild,v,list);
+	}
+	if(!found && r->rchild != NULL){
+	    found = GetNodePath(r->rchild,v,list);
+	}
+	if (!found){
+		list.pop_back();
+	}
+	return found;
+}
+
+//找出二叉树中两个结点的最近公共祖先
+BinNode *LastCommonParent(BinTree r, int v1, int v2) {
+	BinNode *pLast = NULL;   // 最近的公共祖先结点
+	std::vector<BinNode*> list1, list2; // 根结点到v1(v2)结点的路径链表
+	GetNodePath(r, v1, list1);
+	GetNodePath(r, v2, list2);
+	//问题转化为求两个链表的首个公共结点
+	if (list1.empty() || list2.empty())
+		return NULL;
+	std::vector<BinNode*>::iterator iter1, iter2;
+	iter1 = list1.begin();
+	iter2 = list2.begin();
+	while (iter1 != list1.end() && iter2 != list2.end()) {
+		if (*iter1 == *iter2) {
+			pLast = *iter1;
+		}
+		iter1++;
+		iter2++;
+	}
+	return pLast;
+}
+
 void testProblems() {
+	BinTree r, s;
+	freopen("input/BinTree1.txt", "r", stdin);
+	r = Create_BinTree();
+	s = Create_BinTree();
+	printf("%d\n", IsSubTree(r, s));
 	freopen("input/BinTree2.txt", "r", stdin);
-	BinTree r;
 	int res = 0;
 	r = Create_BinTree();
 	MaxPathSum(r, res);
@@ -100,4 +191,18 @@ void testProblems() {
 	printf("%d\n", MaxDepth(r));
 	printf("%d\n", IsAvlTree(r));
 	printf("%d\n", IsSymmetricTree(r));
+	freopen("input/BinTree4.txt", "r", stdin);
+	r = Create_BinTree();
+	PreOrder(r);
+	printf("\n");
+	MirrorTree(r);
+	PreOrder(r);
+	printf("\n");
+	freopen("input/BinTree3.txt", "r", stdin);
+	r = Create_BinTree();
+	BinNode *p = LastCommonParent(r, 10, 13);
+	if (p) {
+		printf("%d\n", ToInt(p));
+	}
+	printf("\n");
 }
